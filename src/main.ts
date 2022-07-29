0bcafb345690c6ae5076c48d85e5aa678d9d87e9
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2022-07-28 19:41:26 -0500

    scroll coment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import './style.css'
 
-
 const menuBtn = document.querySelector<HTMLButtonElement>('.menu-btn');
 const navMenu = document.querySelector<HTMLButtonElement>('.nav-menu');
 
@@ -8,7 +7,10 @@ menuBtn?.addEventListener('click', () =>{
     navMenu?.classList.toggle('show');
 });
 
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.news-cards',{delay : 500});
-ScrollReveal().reveal('.cards-banner-one',{delay : 500});
-ScrollReveal().reveal('.cards-banner-two',{delay : 500});
+
+// ScrollReveal().reveal('.showcase');
+// ScrollReveal().reveal('.news-cards',{delay : 500});
+// ScrollReveal().reveal('.cards-banner-one',{delay : 500});
+// ScrollReveal().reveal('.cards-banner-two',{delay : 500});
+
+
