d357500b81ed5d8c189a3aff4e9ca3fd0000d94b
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2022-07-28 19:25:38 -0500

    pageFazt

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import './style.css'
+
+
+const menuBtn = document.querySelector<HTMLButtonElement>('.menu-btn');
+const navMenu = document.querySelector<HTMLButtonElement>('.nav-menu');
+
+menuBtn?.addEventListener('click', () =>{
+    navMenu?.classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards',{delay : 500});
+ScrollReveal().reveal('.cards-banner-one',{delay : 500});
+ScrollReveal().reveal('.cards-banner-two',{delay : 500});
